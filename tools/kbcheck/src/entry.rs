bc5035d0aba13b20857d66fb95c3f0d0de80d886
James Graham <james@hoppipolla.co.uk>
2022-06-29 15:37:24 +0100

    Improve validator logic
    This splits validation into a few more clearly defined stages and adds
    some additional validators:
    
    * First we read the data files to a string and validate any textual
    properties of the file.
    * Then we validate against the schema and, if that succeeds, convert
    the data into an internal representation.
    * Then we collect the internal representations of all files and
    validate any global properties e.g. uniqueness of certain fields.
    
    This design depends on us being able to load all the data at once;
    that should continue to be true for a long time, but in case it isn't
    we can change the approach for cross-file validation.
    
    Two specific new validation steps are added:
    * Check for trailing whitespace in files.
    * Check for globally unique issue URL (i.e. we can't have multiple kb
    entries for a single platform bug).

---
+++ tools/kbcheck/src/entry.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Severity {
+    Critical,
+    High,
+    Normal,
+    Low,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum UserBaseImpact {
+    Large,
+    Medium,
+    Small,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+#[serde(deny_unknown_fields)]
+pub struct Solutions {
+    #[serde(default)]
+    interventions: Vec<Url>,
+    #[serde(default)]
+    notes: Vec<String>,
+    #[serde(default)]
+    workarounds: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+#[serde(deny_unknown_fields)]
+pub struct References {
+    #[serde(default)]
+    pub breakage: Vec<Url>,
+    #[serde(default)]
+    pub platform_issues: Vec<Url>,
+    #[serde(default)]
+    pub telemetry: Vec<Url>,
+    #[serde(default)]
+    pub testcases: Vec<String>, // TODO: how to handle relative URLs?
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Entry {
+    pub title: String,
+    pub severity: Severity,
+    pub user_base_impact: Option<UserBaseImpact>,
+    #[serde(default)]
+    pub tags: Vec<String>,
+    #[serde(default)]
+    pub symptoms: Vec<String>,
+    #[serde(default)]
+    pub console_messages: Vec<String>,
+    #[serde(default)]
+    pub solutions: Solutions,
+    #[serde(default)]
+    pub references: References,
+}
