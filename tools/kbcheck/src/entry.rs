9236720ab9af9734bfbb6279d83c430d66e1f84d
James Graham <james@hoppipolla.co.uk>
2022-07-22 16:23:51 +0100

    Add detailed site-breakage data
    This is intended to allow us to compute the overall priority of an
    issue based on the factors we use in triage, rather than assigning the
    impact/severity directly.
    
    It adds a number of fields under 'breakage' that contain details about
    the extent and impact of the breakage.
    
    For now plain URLs are still allowed, to avoid having to rewrite all
    existing entries at once.

--- tools/kbcheck/src/entry.rs
+++ tools/kbcheck/src/entry.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use url::Url;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Severity {
     Critical,
@@ -10,7 +10,7 @@ pub enum Severity {
     Low,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum UserBaseImpact {
     Large,
@@ -23,18 +23,18 @@ pub enum UserBaseImpact {
 #[serde(deny_unknown_fields)]
 pub struct Solutions {
     #[serde(default)]
-    interventions: Vec<Url>,
+    pub interventions: Vec<Url>,
     #[serde(default)]
-    notes: Vec<String>,
+    pub notes: Vec<String>,
     #[serde(default)]
-    workarounds: Vec<String>,
+    pub workarounds: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 #[serde(deny_unknown_fields)]
 pub struct References {
     #[serde(default)]
-    pub breakage: Vec<Url>,
+    pub breakage: Vec<BreakageItem>,
     #[serde(default)]
     pub platform_issues: Vec<Url>,
     #[serde(default)]
@@ -45,6 +45,73 @@ pub struct References {
     pub standards_positions: Vec<Url>,
     #[serde(default)]
     pub standards_discussions: Vec<Url>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum BreakageItem {
+    Url(Url),
+    Breakage(Breakage),
+}
+
+impl BreakageItem {
+    pub fn url(&self) -> &Url {
+        match self {
+            BreakageItem::Url(url) => url,
+            BreakageItem::Breakage(item) => &item.url,
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Breakage {
+    pub url: Url,
+    pub site: Url,
+    pub platform: Vec<Platform>,
+    pub last_reproduced: Option<String>, // Date
+    pub intervention: Option<Url>,
+    pub impact: Impact,
+    pub affects_users: AffectsUsers,
+    pub resolution: Option<BreakageResolution>,
+    #[serde(default)]
+    pub notes: String,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Platform {
+    All,
+    Desktop,
+    Mobile,
+    Windows,
+    Macos,
+    Linux,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Impact {
+    SiteBroken,
+    FeatureBroken,
+    SignificantVisual,
+    MinorVisual,
+    UnsupportedMessage,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum AffectsUsers {
+    All,
+    Some,
+    Few,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum BreakageResolution {
+    SiteChanged,
+    SiteFixed,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
